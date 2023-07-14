37dfd17eaad88b1c788ae7c93bc887f3d4f8ecd0
Yi Wang <yiwangyolo@hotmail.com>
2023-07-13 21:07:42 -0400

    section-8 finished

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -14,11 +16,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json()); // middleware is a function that modifying the incoming data
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   console.log('Hello from the middleware. ');
-//   next();
-// });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -27,6 +24,12 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 // SERVER
 module.exports = app;
